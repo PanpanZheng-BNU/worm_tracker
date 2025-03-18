a5122e222928930136b8729b7909100679ae86e9
Panpan Zheng <1293456247@qq.com>
2025-03-18 10:12:18 +0800

    change from mac

--- tracker/iou.py
+++ tracker/iou.py
@@ -24,60 +24,51 @@ def iou(box1, box2):
     return intersection / union
 
 
-def simple_iou_tracker(detections, t_min, sigma_iou=0.3):
-    """
-    simple IOU based tracker.
-    Args:
-        detections(list): list of detections per frame,
-        sigma_l(float): low detection threshold
-        sigma_h(float): high detection threshold
-        sigma_iou(float): IOU threshold
-        t_min (float): minimum track length in frames.
+# def simple_iou_tracker(detections, t_min, sigma_iou=0.3):
+#     """
+#     simple IOU based tracker.
+#     Args:
+#         detections(list): list of detections per frame,
+#         sigma_l(float): low detection threshold
+#         sigma_h(float): high detection threshold
+#         sigma_iou(float): IOU threshold
+#         t_min (float): minimum track length in frames.
 
-    Returns:
-        list: list of trackes.
-    """
-    tracks_active = []
-    tracks_finished = []
-    indx = 0
-    for frame_num, detection_frame in enumerate(detections, start=1):
-        dets = df2list(detection_frame)
+#     Returns:
+#         list: list of trackes.
+#     """
+#     tracks_active = []
+#     tracks_finished = []
+#     indx = 0
+#     for frame_num, detection_frame in enumerate(detections, start=1):
+#         dets = df2list(detection_frame)
 
-        updated_tracks = []
-        for track in tracks_active:
-            if len(dets) > 0:
-                best_match = max(dets, key=lambda x: iou(track['bboxes'][-1], x['bbox']))
-                if iou(track['bboxes'][-1], best_match['bbox']) >= sigma_iou:
-                    track['bboxes'].append(best_match['bbox'])
-                    track['centroids'].append(best_match['centroid'])
-                    track['end_frame'] = frame_num
+#         updated_tracks = []
+#         for track in tracks_active:
+#             if len(dets) > 0:
+#                 best_match = max(dets, key=lambda x: iou(track['bboxes'][-1], x['bbox']))
+#                 if iou(track['bboxes'][-1], best_match['bbox']) >= sigma_iou:
+#                     track['bboxes'].append(best_match['bbox'])
+#                     track['centroids'].append(best_match['centroid'])
+#                     track['end_frame'] = frame_num
 
-                    updated_tracks.append(track)
+#                     updated_tracks.append(track)
 
-                    del dets[[i for i, _ in enumerate(dets) if all(_['centroid'] == best_match['centroid'])][0]]
-                    # dets.index(best_match)
-            if len(updated_tracks) == 0 or track is not updated_tracks[-1]:
-                if len(track['bboxes']) >= t_min:
-                    tracks_finished.append(track)
-        new_tracks = [{'bboxes': [det['bbox']],
-                       'centroids': [det['centroid']],
-                       'start_frame': frame_num,
-                       'end_frame': frame_num} for det in dets]
-        tracks_active = updated_tracks + new_tracks
-    tracks_finished += [track for track in tracks_active
-                        if len(track['bboxes']) >= t_min]
-    return tracks_finished
+#                     del dets[[i for i, _ in enumerate(dets) if all(_['centroid'] == best_match['centroid'])][0]]
+#                     # dets.index(best_match)
+#             if len(updated_tracks) == 0 or track is not updated_tracks[-1]:
+#                 if len(track['bboxes']) >= t_min:
+#                     tracks_finished.append(track)
+#         new_tracks = [{'bboxes': [det['bbox']],
+#                        'centroids': [det['centroid']],
+#                        'start_frame': frame_num,
+#                        'end_frame': frame_num} for det in dets]
+#         tracks_active = updated_tracks + new_tracks
+#     tracks_finished += [track for track in tracks_active
+#                         if len(track['bboxes']) >= t_min]
+#     return tracks_finished
 
 
-def find_csvs(p2result):
-    """
-    """
-    re_num = r"frame(\d+).csv"
-    for root, dirs, files in os.walk(p2result):
-        csvs = [file for file in files if (".csv" in file) and file[0] != "."]
-        csvs = [os.path.join(root, csv) for csv in csvs]
-    csvs.sort(key=lambda x: int(re.findall(re_num, x)[0]))
-    return csvs
 
 def df2list(df):
     """
@@ -137,15 +128,15 @@ def simple_iou_tracker(detections, t_min, sigma_iou=0.3):
                         if len(track['bboxes']) >= t_min]
     return tracks_finished
 
-def Repeat(x):
-    _size = len(x)
-    repeated = []
-    for i in range(_size):
-        k = i + 1
-        for j in range(k, _size):
-            if x[i] == x[j] and x[i] not in repeated:
-                repeated.append(x[i])
-    return repeated
+# def Repeat(x):
+#     _size = len(x)
+#     repeated = []
+#     for i in range(_size):
+#         k = i + 1
+#         for j in range(k, _size):
+#             if x[i] == x[j] and x[i] not in repeated:
+#                 repeated.append(x[i])
+#     return repeated
                 
 def bboxes2indx(df, trackers):
     """
